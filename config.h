52c5dd66d3364c229577e08e5e745fc29427ea48
Lynn <86744728+Layotu@users.noreply.github.com>
2024-08-20 10:30:23 -0500

    Add files via upload

--- config.h
+++ config.h
@@ -1,12 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass = "Polybar";     /* Alternate bar class name */
+static const char *altbarcmd  = "polybar -c $HOME/.config/polybar/config.ini layotu"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const char col_border[]	    = "#000b1e";
+static const char col_black[]	    = "#000000";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -14,8 +19,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#00ddee";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray2, col_gray1,  col_black },
+	[SchemeSel]  = { col_gray3, col_gray1,  col_black },
 };
 
 /* tagging */
@@ -29,11 +34,13 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Firefox",	NULL,	"Firefox",	1 << 8,		0,	-1 },
 	{ "Chromium",   NULL,  	"Chromium",	0,		0,	-1 },
-	{ "Alacritty", 	NULL,	"Alacritty",	0,		0,	-1 }
+	{ "Alacritty", 	NULL,	"Alacritty",	0,		0,	-1 },
+	{ "Conky",	NULL,	"NULL",		0,		1,	-1 },
+	{ "Polybar",	NULL,	NULL,		~0,		1,	-1 }
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.51; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
@@ -60,11 +67,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char *browsercmd[] = { "chromium", NULL };
+static const char *barcmd[] = { "polybar -c $HOME/.config/polybar/config.ini layotu", NULL };
+
+/* patches */
+#include "shiftview.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ Mod1Mask|ControlMask,         XK_t,      spawn,          {.v = termcmd } },
+	{ ControlMask,			XK_n,	   spawn,	   {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -73,8 +86,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             XK_c,      killclient,     {0} },
+	{ MODKEY,        	   	XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -96,6 +108,8 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,           		XK_Tab,   shiftview,      { 1 }},
+	{ MODKEY|ShiftMask,           	XK_Tab,   shiftview,      { -1 }},
 };
 
 /* button definitions */
@@ -114,4 +128,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
